df981b58cc05ff318ddbc1f21f6d735ce3b93daa
thetalemon <sasami0314@gmail.com>
2021-11-01 23:54:29 +0900

    chore: refactoring

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"os"
 	"net/http"
-	"log"
 	"strconv"
 	"github.com/thoas/go-funk"
 	"github.com/line/line-bot-sdk-go/linebot"
@@ -17,13 +16,51 @@ func main() {
 	mainRoutin()
 }
 
+// メイン
 func mainRoutin(){
-	err := godotenv.Load(".env")
-	
-	if err != nil {
-		fmt.Printf("読み込み出来ませんでした: %v", err)
-	} 
+	if err := initLoadEnv(); err != nil {
+		fmt.Errorf("Error: load env:", err)
+		return 
+	}
 
+	response, err := getTasks()
+	if err != nil {
+		fmt.Println("Error: getTasks: ", err)
+		return
+	}
+
+	tasks, err := parseToTasks(response)
+	if err != nil {
+		fmt.Println("Error: parse to tasks:", err)
+		return
+	}
+	
+	bot, err := initLineBot()
+	if err != nil {
+		fmt.Println("Error: init line bot:", err)
+		return
+	}
+	
+	inboxTasksNum := getTasksNum(tasks)
+	text := strconv.Itoa(inboxTasksNum) + "個、タスクが残ってるよ！"
+
+	if err := sendMessageToLine(text, bot); err != nil {
+		fmt.Println("Error: line bot: send message:", err)
+		return 
+	}
+	return
+}
+
+// 環境変数のロードの初期設定
+func initLoadEnv() error {
+	if err := godotenv.Load(".env"); err != nil {
+		return fmt.Errorf("Error: load env:", err)
+	}
+	return nil
+}
+
+// tasksをTodoistから取得
+func getTasks() ([]byte, error) {
 	url := "https://api.todoist.com/rest/v1/tasks"
 	authHeaderName := "Authorization"
 	authHeaderValue := "Bearer " + os.Getenv("TODOIST_TOKEN")
@@ -31,47 +68,62 @@ func mainRoutin(){
 	req, _ := http.NewRequest(http.MethodGet, url, nil)
 	req.Header.Set(authHeaderName, authHeaderValue)
 
-
 	client := new(http.Client)
 	resp, err := client.Do(req)
-
 	if err != nil {
-		fmt.Println("Error Request:", err)
-		return
+		return nil, fmt.Errorf("Error: Request:", err)
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
-			fmt.Println("Error Response:", resp.Status)
-			return
+		return nil, fmt.Errorf("Error: Response:", resp.Status)
 	}
 
 	body, _ := io.ReadAll(resp.Body)
 
-	var articles []Task
-	if err := json.Unmarshal(body, &articles); err != nil {
-		fmt.Println("JSON Unmarshal error:", err)
+	return body, nil
+}
+
+// レスポンスを []Task に変換
+func parseToTasks(response []byte) ([]Task, error)  {
+	var tasks []Task
+	err := json.Unmarshal(response, &tasks)
+	if err != nil {
+		return nil, fmt.Errorf("Error: JSON Unmarshal:", err)
 	}
+
+	return tasks, nil
+}
+
+// Linebotの初期設定
+func initLineBot() (*linebot.Client, error) {
 	bot, err := linebot.New(
 		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
 		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
 	)
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("Error: creaate line bot:", err)
 	}
+	return bot, nil
+}
 
-	inboxTasks := funk.Filter(articles, func(x Task) bool {
+// INBOXのタスク個数の取得
+func getTasksNum(tasks []Task) int {
+	inboxTasks := funk.Filter(tasks, func(x Task) bool {
     return x.ProjectID == 1528596419
 	})
-	inboxTasksNum := len(inboxTasks.([]Task))
+	return len(inboxTasks.([]Task))
+}
 
-	text := strconv.Itoa(inboxTasksNum) + "個、タスクが残ってるよ！"
+// メッセージをLINEに送る
+func sendMessageToLine(text string, bot *linebot.Client) error  {
 	message := linebot.NewTextMessage(text)
-
-	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
-		log.Fatal(err)	
+	_, err := bot.BroadcastMessage(message).Do()
+	if err != nil {
+		return fmt.Errorf("Error: line bot: boradcast message:", err)
 	}
+	return nil
 }
 
 type Task struct {
